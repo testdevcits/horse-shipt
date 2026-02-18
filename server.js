45e5dcad1248097f3a54d5a1bcf913dc62849def
testdevcits <testdevcits@gmail.com>
2026-02-18 18:42:26 +0530

    feat(shipper): add separate map-only API for available shipments

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use(
       return callback(new Error("CORS policy: Origin not allowed"));
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   })
 );
 
